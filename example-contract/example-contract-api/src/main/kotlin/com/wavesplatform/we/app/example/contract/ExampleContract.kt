f7f50b3211ff9aaad06fb6579e2b2e3c974c8181
krenovut <krenovut@gmail.com>
2021-03-31 06:03:10 +0300

    initial commit

--- example-contract/example-contract-api/src/main/kotlin/com/wavesplatform/we/app/example/contract/ExampleContract.kt
+++ example-contract/example-contract-api/src/main/kotlin/com/wavesplatform/we/app/example/contract/ExampleContract.kt
@@ -2,6 +2,7 @@ package com.wavesplatform.we.app.example.contract
 
 import com.wavesplatform.vst.contract.ContractAction
 import com.wavesplatform.vst.contract.ContractInit
+import com.wavesplatform.vst.contract.InvokeParam
 
 interface ExampleContract {
 
@@ -9,5 +10,21 @@ interface ExampleContract {
     fun create()
 
     @ContractAction
-    fun invoke()
+    fun createCompany(
+            @InvokeParam(name="name") name: String,
+            @InvokeParam(name="data") data: String
+    )
+    @ContractAction
+    fun createSupply(
+            @InvokeParam(name="id") id: String
+    )
+    @ContractAction
+    fun transport(
+            @InvokeParam(name="id") id: String
+    )
+    @ContractAction
+    fun finishSupply(
+            @InvokeParam(name="id") id: String
+    )
+
 }
--- example-contract/example-contract-app/src/main/kotlin/com/wavesplatform/we/app/example/contract/impl/ExampleContractImpl.kt
+++ example-contract/example-contract-app/src/main/kotlin/com/wavesplatform/we/app/example/contract/impl/ExampleContractImpl.kt
@@ -1,5 +1,7 @@
 package com.wavesplatform.we.app.example.contract.impl
 
+import com.wavesplatform.vst.contract.data.ContractCall
+import com.wavesplatform.vst.contract.mapping.Mapping
 import com.wavesplatform.vst.contract.spring.annotation.ContractHandlerBean
 import com.wavesplatform.vst.contract.state.ContractState
 import com.wavesplatform.vst.contract.state.setValue
@@ -8,17 +10,92 @@ import com.wavesplatform.we.app.example.contract.ExampleContract
 
 @ContractHandlerBean
 class ExampleContractImpl(
-    state: ContractState
+    state: ContractState,
+    private val call:ContractCall
 ) : ExampleContract {
 
     private var create: Boolean? by state
-    private var invoke: Boolean? by state
+
+    private val companies: Mapping<Company> by state
+    private val supplies: Mapping<Supply> by state
 
     override fun create() {
         create = true
     }
 
-    override fun invoke() {
-        invoke = true
+    override fun createCompany(name: String, data: String) {
+        val owner = call.sender
+        require(!companies.has(owner)) {
+            "COMPANY_ALREADY_EXISTS"
+        }
+        val company = Company(
+                name=name,
+                data=data,
+                owner = owner
+        )
+        companies.put(owner, company)
+    }
+
+    override fun createSupply(id:String) {
+        val owner = call.sender
+        require(companies.has(owner)) {
+            "COMPANY_NOT_EXISTS"
+        }
+        val supply = Supply(
+                id =id,
+                comp1 =owner,
+                start_time = call.timestamp
+        )
+        supplies.put(id, supply)
+    }
+
+    override fun transport(id: String) {
+        val owner = call.sender
+        require(companies.has(owner)) {
+            "COMPANY_NOT_EXISTS"
+        }
+        val company = companies[owner]
+        require(company.name=="Transport") {
+            "WRONG_COMPANY"
+        }
+        val supply = supplies[id]
+        val updateSupply = supply.copy(
+                transport=owner,
+                transport_time = call.timestamp
+        )
+        supplies.put(id, updateSupply)
+    }
+
+    override fun finishSupply(id:String) {
+        val owner = call.sender
+        require(companies.has(owner)) {
+            "COMPANY_NOT_EXISTS"
+        }
+        val company = companies[owner]
+        require(company.name=="comp2") {
+            "WRONG_COMPANY"
+        }
+        val supply = supplies[id]
+        val updateSupply = supply.copy(
+                comp2=owner,
+                finish_time = call.timestamp
+        )
+        supplies.put(id, updateSupply)
     }
 }
+
+data class Supply(
+        val id: String,
+        val comp1: String,
+        val start_time: Long,
+        val comp2: String? = null,
+        val transport: String? = null,
+        val transport_time: Long? = null,
+        val finish_time: Long? = null
+)
+
+data class Company(
+        val name: String,
+        val data: String,
+        val owner: String
+)
